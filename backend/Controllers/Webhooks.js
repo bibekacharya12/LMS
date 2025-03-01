0219aac346f02d624628cd4b72f310235f6267a7
Bibek <bbk.acharyaaa@gmail.com>
2025-03-01 14:46:54 +0545

    Webhook.js file updated

--- backend/Controllers/Webhooks.js
+++ backend/Controllers/Webhooks.js
@@ -6,7 +6,7 @@ import User from "../Models/User.model.js";
 export const clerkWebhooks = async (req, res) => {
   try {
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
-    await whook.verify(JSON.stringify(req, body), {
+    await whook.verify(JSON.stringify(req.body), {
       "svix-id": req.headers["svix-id"],
       "svix-timestamp": req.headers["svix-timestamp"],
       "svix-signature": req.headers["svix-signature"],
